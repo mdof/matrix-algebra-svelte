7afc037a05a18259a011955996308b2629eba66f
esf <efrain.sotelo.ferry@gmail.com>
2022-01-03 21:29:45 +0100

    feat:MatrixSquare

---
+++ src/lib/MatrixSquare.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Matrix from './Matrix.svelte';
+	import { getDimMatrix } from './scripts/matrix';
+
+	export let matrix_vec: number[] = [];
+	export let flow_order: 'column' | 'row' = 'column';
+	export let disabled: number[] = [];
+
+	let dim_matrix: number = getDimMatrix(matrix_vec);
+	$: if (matrix_vec) {
+		dim_matrix = getDimMatrix(matrix_vec);
+	}
+</script>
+
+<Matrix rows={dim_matrix} cols={dim_matrix} bind:matrix_vec {flow_order} {disabled} />
