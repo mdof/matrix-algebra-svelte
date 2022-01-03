db649234d49011801e26db8f1cd69c56ea05c602
esf <efrain.sotelo.ferry@gmail.com>
2022-01-03 15:33:14 +0100

    refactor: update test cases

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+	//
 	import Matrix from '$lib/Matrix.svelte';
 	let vec = [10, 20, 30, 40];
 	let not_squared = [1, 2, 3];
-
+	$: console.log(`INDEX ${vec}`);
+	//
 	let dim = 2;
 	let vec_modified_by_dim = Array(dim * dim).fill(0);
 	$: if (dim) {
@@ -11,28 +13,36 @@
 	}
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="font-bold">Welcome to matrix-algebra-svelte</h1>
 
-<div class="w-[80%]">
+<div class="block">
+	<h2 class="font-bold">Reactivity</h2>
 	<h2>Vector = {vec}</h2>
-	<h2>Full matrx , flow column (default)</h2>
-	<Matrix matrix_vec={vec} />
-	<h2>Full matrix , flow row</h2>
-	<Matrix matrix_vec={vec} flow_order="row" />
-	<h2>Not squared</h2>
-	<Matrix matrix_vec={not_squared} />
+	<Matrix bind:matrix_vec={vec} />
+</div>
+<div class="block">
+	<h2 class="font-bold">Full matrix , flow column (default)</h2>
+	<Matrix matrix_vec={[1, 2, 3, 4]} />
+	<h2 class="font-bold">Full matrix , flow row</h2>
+	<Matrix matrix_vec={[1, 2, 3, 4]} flow_order="row" />
+	<h2 class="font-bold">Not squared</h2>
+	<Matrix bind:matrix_vec={not_squared} />
+</div>
 
+<div class="block">
+	<h2 class="font-bold">Dynamic dimension</h2>
 	<div>
-		<h2>Dynamic dimension</h2>
-		<div>
-			<span>Dim</span>
-			<span>{dim}</span>
-			<button class="w-6 bg-red-100" on:click={() => (dim -= 1)}>-</button>
-			<button class="w-6 bg-red-100" on:click={() => (dim += 1)}>+</button>
-		</div>
-		<Matrix matrix_vec={vec_modified_by_dim} />
+		<span>Dim</span>
+		<span>{dim}</span>
+		<button class="w-6 bg-red-100" on:click={() => (dim -= 1)}>-</button>
+		<button class="w-6 bg-red-100" on:click={() => (dim += 1)}>+</button>
 	</div>
+	<Matrix bind:matrix_vec={vec_modified_by_dim} />
 </div>
 
 <!-- TODO: Add rows and cols input if squared matrix not desired -->
+<style>
+	.block {
+		@apply w-[50%] p-10 border-2 border-gray-100 rounded-xl my-14 mx-auto;
+	}
+</style>
