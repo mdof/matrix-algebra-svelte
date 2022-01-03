2a6384d7cfc9483a12c1a64cdb9b38674260ecc9
esf <efrain.sotelo.ferry@gmail.com>
2022-01-03 16:17:31 +0100

    test:parseNumber adding NaN result test

--- src/lib/scripts/parseStringToNumber.test.ts
+++ src/lib/scripts/parseStringToNumber.test.ts
@@ -9,6 +9,8 @@ describe('parseNumber', () => {
 		//Error input but parse is forgiven
 		expect(parseNumber('1000..2')).toBeCloseTo(val, 4);
 		expect(parseNumber('0.1000.2')).toBeCloseTo(val, 4);
+		//Return nan
+		expect(parseNumber('aaa')).toBeNaN();
 	});
 	test('scientific format input', () => {
 		expect(parseNumber('1.0002e3')).toBeCloseTo(val, 4);
