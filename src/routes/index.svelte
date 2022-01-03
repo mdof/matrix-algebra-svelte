37cd36cd582ee8f67c299bda0429d2edd10321c5
esf <efrain.sotelo.ferry@gmail.com>
2022-01-03 16:19:20 +0100

    refactor:updated with MatrixSymm component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	//
 	import Matrix from '$lib/Matrix.svelte';
+	import MatrixSymm from '$lib/MatrixSymm.svelte';
 	let vec = [10, 20, 30, 40];
-	let not_squared = [1, 2, 3];
+	let not_squared = [1, 2, 3, 4, 5, 6];
 	$: console.log(`INDEX ${vec}`);
 	//
 	let dim = 2;
@@ -18,15 +19,15 @@
 <div class="block">
 	<h2 class="font-bold">Reactivity</h2>
 	<h2>Vector = {vec}</h2>
-	<Matrix bind:matrix_vec={vec} />
+	<MatrixSymm bind:matrix_vec={vec} />
 </div>
 <div class="block">
-	<h2 class="font-bold">Full matrix , flow column (default)</h2>
-	<Matrix matrix_vec={[1, 2, 3, 4]} />
-	<h2 class="font-bold">Full matrix , flow row</h2>
-	<Matrix matrix_vec={[1, 2, 3, 4]} flow_order="row" />
+	<h2 class="font-bold">Square matrix , flow column (default)</h2>
+	<MatrixSymm matrix_vec={[1, 2, 3, 4]} />
+	<h2 class="font-bold">Square matrix , flow row</h2>
+	<MatrixSymm matrix_vec={[1, 2, 3, 4]} flow_order="row" />
 	<h2 class="font-bold">Not squared</h2>
-	<Matrix bind:matrix_vec={not_squared} />
+	<Matrix matrix_vec={[1, 2, 3, 4, 5, 6]} rows={2} cols={3} />
 </div>
 
 <div class="block">
@@ -37,11 +38,11 @@
 		<button class="w-6 bg-red-100" on:click={() => (dim -= 1)}>-</button>
 		<button class="w-6 bg-red-100" on:click={() => (dim += 1)}>+</button>
 	</div>
-	<Matrix bind:matrix_vec={vec_modified_by_dim} />
+	<MatrixSymm bind:matrix_vec={vec_modified_by_dim} />
 </div>
 
 <!-- TODO: Add rows and cols input if squared matrix not desired -->
-<style>
+<style style="postcss">
 	.block {
 		@apply w-[50%] p-10 border-2 border-gray-100 rounded-xl my-14 mx-auto;
 	}
