b034040878f9c454a1c0fdfc260baa417f30fb6d
esf <efrain.sotelo.ferry@gmail.com>
2022-01-06 23:30:20 +0100

    feat:scripts support for symm matrix

--- src/lib/scripts/matrix.test.ts
+++ src/lib/scripts/matrix.test.ts
@@ -1,4 +1,10 @@
-import { getDimMatrix, getDisabled } from './matrix';
+import {
+	getDimMatrix,
+	getDisabled,
+	getPosition,
+	getRowColPosition,
+	getSymmetricPosition
+} from './matrix';
 
 describe('square matrix', () => {
 	test('get dim of matrix_vec', () => {
@@ -13,5 +19,62 @@ describe('symmetric matrix', () => {
 		expect(getDisabled(2)).toEqual([1]);
 		expect(getDisabled(3)).toEqual([1, 2, 5]);
 		expect(getDisabled(4)).toEqual([1, 2, 3, 6, 7, 11]);
+	});
+	test('get symmetric position', () => {
+		//Matrix 2x2
+		expect(getSymmetricPosition(2, 'col', 0)).toBe(0);
+		expect(getSymmetricPosition(2, 'col', 1)).toBe(2);
+		expect(getSymmetricPosition(2, 'col', 2)).toBe(1);
+		expect(getSymmetricPosition(2, 'col', 3)).toBe(3);
+		expect(getSymmetricPosition(2, 'row', 0)).toBe(0);
+		expect(getSymmetricPosition(2, 'row', 1)).toBe(2);
+		expect(getSymmetricPosition(2, 'row', 2)).toBe(1);
+		//Matrix 3x3
+		expect(getSymmetricPosition(3, 'col', 0)).toBe(0);
+		expect(getSymmetricPosition(3, 'col', 1)).toBe(3);
+		expect(getSymmetricPosition(3, 'col', 2)).toBe(6);
+		expect(getSymmetricPosition(3, 'col', 3)).toBe(1);
+		expect(getSymmetricPosition(3, 'col', 4)).toBe(4);
+		expect(getSymmetricPosition(3, 'col', 5)).toBe(7);
+		expect(getSymmetricPosition(3, 'col', 6)).toBe(2);
+		expect(getSymmetricPosition(3, 'col', 7)).toBe(5);
+		expect(getSymmetricPosition(3, 'col', 8)).toBe(8);
+	});
+});
+
+describe('utils', () => {
+	test('getRowColPosition', () => {
+		//Matrix 2x2
+		expect(getRowColPosition(2, 'col', 0)).toEqual([0, 0]);
+		expect(getRowColPosition(2, 'col', 1)).toEqual([1, 0]);
+		expect(getRowColPosition(2, 'col', 2)).toEqual([0, 1]);
+		expect(getRowColPosition(2, 'col', 3)).toEqual([1, 1]);
+		expect(getRowColPosition(2, 'row', 0)).toEqual([0, 0]);
+		expect(getRowColPosition(2, 'row', 1)).toEqual([0, 1]);
+		expect(getRowColPosition(2, 'row', 2)).toEqual([1, 0]);
+		expect(getRowColPosition(2, 'row', 3)).toEqual([1, 1]);
+		//Matrix 3x3
+		expect(getRowColPosition(3, 'col', 0)).toEqual([0, 0]);
+		expect(getRowColPosition(3, 'col', 1)).toEqual([1, 0]);
+		expect(getRowColPosition(3, 'col', 2)).toEqual([2, 0]);
+		expect(getRowColPosition(3, 'col', 3)).toEqual([0, 1]);
+		expect(getRowColPosition(3, 'col', 4)).toEqual([1, 1]);
+		expect(getRowColPosition(3, 'col', 5)).toEqual([2, 1]);
+		expect(getRowColPosition(3, 'col', 6)).toEqual([0, 2]);
+		expect(getRowColPosition(3, 'col', 7)).toEqual([1, 2]);
+		expect(getRowColPosition(3, 'col', 8)).toEqual([2, 2]);
+		expect(getRowColPosition(3, 'row', 0)).toEqual([0, 0]);
+		expect(getRowColPosition(3, 'row', 1)).toEqual([0, 1]);
+		expect(getRowColPosition(3, 'row', 2)).toEqual([0, 2]);
+		expect(getRowColPosition(3, 'row', 3)).toEqual([1, 0]);
+		expect(getRowColPosition(3, 'row', 4)).toEqual([1, 1]);
+		expect(getRowColPosition(3, 'row', 5)).toEqual([1, 2]);
+		expect(getRowColPosition(3, 'row', 6)).toEqual([2, 0]);
+		expect(getRowColPosition(3, 'row', 7)).toEqual([2, 1]);
+		expect(getRowColPosition(3, 'row', 8)).toEqual([2, 2]);
+	});
+	test('get position', () => {
+		expect(getPosition(0, 1, 2, 'col')).toBe(2);
+		expect(getPosition(0, 1, 2, 'row')).toBe(1);
 	});
 });
--- src/lib/scripts/matrix.ts
+++ src/lib/scripts/matrix.ts
@@ -1,11 +1,20 @@
+export type Flow_Order = 'column' | 'row';
+
+export function isSquared(vec: number[]): boolean {
+	let dim_matrix = getDimMatrix(vec);
+	if (typeof dim_matrix == 'undefined') return false;
+	return true;
+}
+
 export function getDimMatrix(vec: number[]): number | undefined {
 	let sqrtDimVec = Math.sqrt(vec.length);
 	if (!Number.isInteger(sqrtDimVec)) {
-		// return Math.round(sqrtDimVec);
 		return undefined;
+		// return Math.round(sqrtDimVec);
 	}
 	return sqrtDimVec;
 }
+
 export function getDisabled(dim: number): number[] {
 	let disabled = [];
 	for (let line = 0; line < dim; line++) {
@@ -18,6 +27,28 @@ export function getDisabled(dim: number): number[] {
 		}
 	}
 	return disabled;
+}
+
+export function getSymmetricPosition(dim: number, flow_order: Flow_Order, pos: number): number {
+	let [row, col] = getRowColPosition(dim, flow_order, pos);
+	let sym = [col, row];
+	return getPosition(sym[0], sym[1], dim, flow_order);
+}
+
+export function getRowColPosition(
+	dim: number,
+	flow_order: Flow_Order,
+	pos: number
+): [number, number] {
+	let mod = pos % dim;
+	let base = (pos - mod) / dim;
+	if (flow_order == 'column') return [mod, base];
+	return [base, mod];
+}
+
+export function getPosition(row: number, col: number, dim: number, flow_order: Flow_Order): number {
+	if (flow_order == 'column') return col * dim + row;
+	return row * dim + col;
 }
 
 function getArray(start: number, end: number): number[] {
