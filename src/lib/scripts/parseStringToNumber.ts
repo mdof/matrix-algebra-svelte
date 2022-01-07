2b1cccbbefbadf15493364527428c5bc6985bb65
esf <efrain.sotelo.ferry@gmail.com>
2022-01-07 12:50:17 +0100

    fix: parseNumber

--- src/lib/scripts/parseStringToNumber.ts
+++ src/lib/scripts/parseStringToNumber.ts
@@ -4,8 +4,8 @@ export function parseNumber(value: string): number {
 	//Only use last separator and delete rest
 	//Don't filter last separator ( sep.length - 2 )
 	for (let i = separators.length - 2; i >= 0; i--) {
-		let idx = separators[i].index;
-		filteredSeparators.splice(idx, 1);
+		let idx: number | undefined = separators[i].index;
+		if (typeof idx !== 'undefined') filteredSeparators.splice(idx, 1);
 	}
 	const normalized = filteredSeparators.join('').replace(',', '.');
 	let parsedNumber = parseFloat(normalized);
