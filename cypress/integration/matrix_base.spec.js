3c1056b74b51c4d3df4bfd3482c86036ce55faca
esf <efrain.sotelo.ferry@gmail.com>
2022-01-10 00:28:21 +0100

    fix: cypress run fails but cypress open works
    -Workaoround use retries to run cypress , headless mode. Also use
    multiple times of test

--- cypress/integration/matrix_base.spec.js
+++ cypress/integration/matrix_base.spec.js
@@ -1,51 +1,48 @@
 /// <reference types="cypress" />
 import { testData } from '../../src/lib/scripts/testFormatNumber.cjs';
 
+// multiple tests to avoid flake tests
+// https://glebbahmutov.com/blog/repeat-to-fight-flake/
+const numTests = 4;
 describe('Input number', () => {
 	beforeEach(() => {
 		cy.visit('/');
+		cy.get('[data-test-id="reactive"]').as('matrix');
 	});
-	it('format on leave', () => {
-		cy.get('[data-test-id="reactive"]').within(() => {
-			cy.get('input').as('inputs');
 
-			cy.get('@inputs').should('have.length', 4);
-			cy.get('@inputs').first().as('input');
+	Cypress._.times(numTests, (k) => {
+		it(`test ${k} of ${numTests} : format on leave`, () => {
+			cy.get('@matrix').within(() => {
+				// cy.wait(2000);
+				cy.get('input').should('have.length', 4).first().as('input');
 
-			cy.get('@input').clear().should('have.value', '');
-			for (let testFormat of testData['formatString']) {
-				cy.get('@input')
-					.focus()
-					.type(testFormat['input'])
-					.blur()
-					.should('have.value', testFormat['output'])
-					.clear();
-				//Negative value
-				cy.get('@input')
-					.focus()
-					.type('-' + testFormat['input'])
-					.blur()
-					.should('have.value', '-' + testFormat['output'])
-					.clear();
-			}
-		});
-	});
-	it('format on leave - zero', () => {
-		cy.get('[data-test-id="reactive"]').within(() => {
-			cy.get('input').as('inputs');
+				cy.get('@input').clear().should('have.value', '');
 
-			cy.get('@inputs').should('have.length', 4);
-			cy.get('@inputs').first().as('input');
-
-			cy.get('@input').clear().should('have.value', '');
-			for (let testFormat of testData['formatStringZero']) {
-				cy.get('@input')
-					.focus()
-					.type(testFormat['input'])
-					.blur()
-					.should('have.value', testFormat['output'])
-					.clear();
-			}
+				for (let testFormat of testData['formatString']) {
+					cy.get('@input')
+						.focus()
+						.type(testFormat['input'])
+						.blur()
+						.should('have.value', testFormat['output'])
+						.clear();
+					//Negative value
+					cy.get('@input')
+						.focus()
+						.type('-' + testFormat['input'])
+						.blur()
+						.should('have.value', '-' + testFormat['output'])
+						.clear();
+				}
+				//Test zeros
+				for (let testFormat of testData['formatStringZero']) {
+					cy.get('@input')
+						.focus()
+						.type(testFormat['input'])
+						.blur()
+						.should('have.value', testFormat['output'])
+						.clear();
+				}
+			});
 		});
 	});
 });
